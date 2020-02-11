ede1c8aa1f5ac12af34ac21a704a9a2e7277146f
City of Vernonia GIS <gis@vernonia-or.gov>
2020-02-11 14:37:44 -0800

    update analytics

--- js/analytics.js
+++ js/analytics.js
@@ -1,4 +1,4 @@
 window.dataLayer = window.dataLayer || [];
 function gtag() { dataLayer.push(arguments); }
 gtag('js', new Date());
-gtag('config', 'UA-123980059-3');
+gtag('config', 'UA-123980059-4');
